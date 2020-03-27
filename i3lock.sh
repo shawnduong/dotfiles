4056235997bb821420b89cd5bd7bdd1b76426623
Shawn Duong <shawnduong@protonmail.com>
2020-03-27 16:46:59 -0700

    Tweaked separator color.

--- i3lock.sh
+++ i3lock.sh
@@ -5,7 +5,7 @@ rm /tmp/screenshot.png /tmp/screenshotblur.png /tmp/screenshotblurlock.png 2>/de
 scrot /tmp/screenshot.png
 convert /tmp/screenshot.png -blur 0x9 /tmp/screenshotblur.png
 convert -gravity center /tmp/screenshotblur.png /usr/share/pixmaps/lock.png -composite /tmp/screenshotblurlock.png
-i3lock -i /tmp/screenshotblurlock.png --indicator --indpos="w/2:h/2+8" --radius 150 --ring-width 10 --greetertext="Authentication Required" --veriftext="" --wrongtext="" --noinputtext="" --locktext="" --lockfailedtext="" --insidevercolor=00000000 --insidewrongcolor=00000000 --insidecolor=00000000 --ringvercolor=b8cffecc --ringwrongcolor=feb8cfcc --ringcolor=000000ff --linecolor=00000000 --keyhlcolor=b8cffecc --bshlcolor=feb8cfcc --separatorcolor=000000ff
+i3lock -i /tmp/screenshotblurlock.png --indicator --indpos="w/2:h/2+8" --radius 150 --ring-width 10 --greetertext="Authentication Required" --veriftext="" --wrongtext="" --noinputtext="" --locktext="" --lockfailedtext="" --insidevercolor=00000000 --insidewrongcolor=00000000 --insidecolor=00000000 --ringvercolor=b8cffecc --ringwrongcolor=feb8cfcc --ringcolor=000000ff --linecolor=00000000 --keyhlcolor=b8cffecc --bshlcolor=feb8cfcc --separatorcolor=00000000
 
 # Sleep to give i3lock time to lock.
 sleep 1
